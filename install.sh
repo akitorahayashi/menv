74b7b4ebca75a60309129dd28ab94abf6865bef7
akitorahayashi <atora.has75@gmail.com>
2025-06-14 19:50:44 +0900

    実行順序や絶対パスを修正

--- install.sh
+++ install.sh
@@ -51,14 +51,14 @@ main() {
     fi
     
     # セットアップスクリプトの実行
-    "$SCRIPT_ROOT_DIR/scripts/setup/homebrew.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/shell.sh"
-    "$SCRIPT_ROOT_DIR/scripts/setup/flutter.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/mac.sh"
+    "$SCRIPT_ROOT_DIR/scripts/setup/homebrew.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/git.sh"
+    "$SCRIPT_ROOT_DIR/scripts/setup/cursor.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/ruby.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/xcode.sh"
-    "$SCRIPT_ROOT_DIR/scripts/setup/cursor.sh"
+    "$SCRIPT_ROOT_DIR/scripts/setup/flutter.sh"
     "$SCRIPT_ROOT_DIR/scripts/setup/node.sh"
 
     # インストール結果の表示
