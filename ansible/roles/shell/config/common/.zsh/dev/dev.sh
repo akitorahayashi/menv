81905d8fb67615e7d9be256331d7f4817d439cb7
akitorahayashi <hayashi20030703@gmail.com>
2025-10-02 11:51:10 +0900

    chore: Update .gitignore and improve development scripts; restructure dependencies in pyproject.toml and uv.lock

--- ansible/roles/shell/config/common/.zsh/dev/dev.sh
+++ ansible/roles/shell/config/common/.zsh/dev/dev.sh
@@ -17,27 +17,27 @@ dev_alias_as() {
 	fi
 
 	# Common development commands
-	alias "${prefix}-h=\${cmd_prefix} help"
-	alias "${prefix}-s=\${cmd_prefix} setup"
-	alias "${prefix}-f=\${cmd_prefix} format"
-	alias "${prefix}-l=\${cmd_prefix} lint"
-	alias "${prefix}-r=\${cmd_prefix} run"
-	alias "${prefix}-t=\${cmd_prefix} test"
-	alias "${prefix}-cln=\${cmd_prefix} clean"
+	alias "${prefix}-h=${cmd_prefix} help"
+	alias "${prefix}-s=${cmd_prefix} setup"
+	alias "${prefix}-f=${cmd_prefix} format"
+	alias "${prefix}-l=${cmd_prefix} lint"
+	alias "${prefix}-r=${cmd_prefix} run"
+	alias "${prefix}-t=${cmd_prefix} test"
+	alias "${prefix}-cln=${cmd_prefix} clean"
 
 	# Combined format and lint
-	alias "${prefix}-fl=\${cmd_prefix} format && \${cmd_prefix} lint"
+	alias "${prefix}-fl=${cmd_prefix} format && ${cmd_prefix} lint"
 
 	# Test variations
-	alias "${prefix}-ut=\${cmd_prefix} unit-test"
-	alias "${prefix}-uit=\${cmd_prefix} ui-test"
-	alias "${prefix}-et=\${cmd_prefix} e2e-test"
-	alias "${prefix}-lt=\${cmd_prefix} local-test"
-	alias "${prefix}-dt=\${cmd_prefix} docker-test"
-	alias "${prefix}-sqt=\${cmd_prefix} sqlt-test"
-	alias "${prefix}-pst=\${cmd_prefix} pstg-test"
-	alias "${prefix}-sdt=\${cmd_prefix} sdk-test"
-	alias "${prefix}-pet=\${cmd_prefix} perf-test"
-	alias "${prefix}-it=\${cmd_prefix} intg-test"
-	alias "${prefix}-bt=\${cmd_prefix} build-test"
+	alias "${prefix}-ut=${cmd_prefix} unit-test"
+	alias "${prefix}-uit=${cmd_prefix} ui-test"
+	alias "${prefix}-et=${cmd_prefix} e2e-test"
+	alias "${prefix}-lt=${cmd_prefix} local-test"
+	alias "${prefix}-dt=${cmd_prefix} docker-test"
+	alias "${prefix}-sqt=${cmd_prefix} sqlt-test"
+	alias "${prefix}-pst=${cmd_prefix} pstg-test"
+	alias "${prefix}-sdt=${cmd_prefix} sdk-test"
+	alias "${prefix}-pet=${cmd_prefix} perf-test"
+	alias "${prefix}-it=${cmd_prefix} intg-test"
+	alias "${prefix}-bt=${cmd_prefix} build-test"
 }
