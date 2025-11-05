7ff284e97516a1f41a1cc19b66205698e285f04c
akitorahayashi <hayashi20030703@gmail.com>
2025-11-06 02:33:15 +0900

    feat: Add script to generate VSCode workspace file from specified paths; include usage instructions in aliases

--- ansible/roles/shell/config/common/alias/vscode.sh
+++ ansible/roles/shell/config/common/alias/vscode.sh
@@ -2,3 +2,7 @@
 alias co="code"
 alias co-r="code --reuse-window"
 alias co-n="code --new-window"
+
+# Generate .code-workspace file in current dir
+# Usage: co-w ../path1 /abs/path2
+alias co-w='gen_vscode_workspace.py'
